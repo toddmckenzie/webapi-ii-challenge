468b8ebda772ba9efe3667a5891a540263bc4064
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-05-28 17:49:12 -0500

    code written, need to debug put/delete/posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -44,37 +44,69 @@ router.get('/:id/comments', async (req,res) => {
         res.status(500).json({ message: 'The post inofrmation could not be retrieved'})
     }
 })
+//working
+router.post('/', async (req, res) => {
 
-router.post('/', (req, res) => {
     try {
+        const { title, contents } = req.body
+        if ( !title || !contents ) {
+            res.status(400).json({ message: "Please provide title and contents for posts"})
+        } else {
+            res.status(201).json(req.body)
+        }
 
     } catch (error) {
-
+        res.status(500).json({ message: "There was an error while saving the post to the database" })
     }
 })
-
-router.post('/:id/comments', (req, res) => {
+//come back to this one.  at top of readme.
+router.post('/:id/comments', async (req, res) => {
     try {
+        const id = req.params.id;
+        //const commentsById = await Posts.findPostComments(req.params.id)
+        console.log(id)
+        const { text } = req.body
+        if (!id) {
+            res.status(404).json({ message: "The post with the specified ID does not exist."})
+        } else if (!text) {
+            res.status(400).json({ message: "Please provide text for the comment"})
+        } else {
+            res.status(201).json(req.body)
+        }
 
     }catch (error) {
-
+        res.status(500).json({ message: "There was an error saving the comment to the database"})
     }
 })
 
 
-router.delete('/:id', (req,) => {
+router.delete('/:id', async (req, res) => {
     try {
-
+        const count = await Posts.remove(req.params.id);
+        console.log(count + ' here is count')
+        if (count > 0) {
+            res.status(200).json({message: "It was removed"})
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist."})
+        }
     } catch (error) {
-
+        res.status(500).json({ message: "The post could not be removed."})
     }
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     try {
+        const id = req.params.id;
+        const { title, contents } = req.body;
+        const post = await Posts.update(id, req.body);
+        if (post) {
+            res.status(200).json(req.body)
+        } else {
+            res.status(404).json({ message: "Please provide title and contents for the post." })
+        }
     
     } catch (error) {
-
+        res.status(500).json({error: "The post information could not be modified."})
     }
 })
 
