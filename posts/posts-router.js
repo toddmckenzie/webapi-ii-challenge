4e28d438776e75944a502ae360cfa8b36dfacc3d
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-05-28 16:45:31 -0500

    gets working properly

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const Posts = require('../data/db.js');
-
+//working
 router.get('/', async (req, res) => {
     try {
         console.log(req)
@@ -12,34 +12,36 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: "The posts information could not be retrieved."})
     }
 })
-
+//working
 router.get('/:id', async (req, res) => {
     try {
 
         console.log(req.params.id)
         const postsById = await Posts.findById(req.params.id)
-        if (postsById) {
+        console.log(` here is posts by id ${postsById}`)
+        if (postsById.length > 0) {
+            console.log(postsById)
             res.status(200).json(postsById)
         } else {
-            res.status(400).json({ message: 'Posts not found'})
+            res.status(404).json({ message: 'Posts not found'})
         }
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "The post with the specified ID does not exist."})
     }
 })
-
+//working
 router.get('/:id/comments', async (req,res) => {
     try {
-        console.log(req);
         const commentsById = await Posts.findPostComments(req.params.id)
-        if (commentsById) {
+        if (commentsById.length > 0) {
             res.status(200).json(commentsById)
         } else {
-            res.status(500).json({ message: 'Error retrieving comments'})
+            res.status(404).json({ message: 'The post information could not be retrieved.'})
         }
     } catch (error) {
-
+        console.log(error);
+        res.status(500).json({ message: 'The post inofrmation could not be retrieved'})
     }
 })
 
@@ -54,7 +56,7 @@ router.post('/', (req, res) => {
 router.post('/:id/comments', (req, res) => {
     try {
 
-    }catch {
+    }catch (error) {
 
     }
 })
@@ -63,7 +65,15 @@ router.post('/:id/comments', (req, res) => {
 router.delete('/:id', (req,) => {
     try {
 
-    } catch {
+    } catch (error) {
+
+    }
+})
+
+router.put('/:id', (req, res) => {
+    try {
+    
+    } catch (error) {
 
     }
 })
